ffa6b04ad96ee0e749b7af6639702183994b610e
Regis Laffond <regis.laffond@yahoo.fr>
2024-04-29 10:49:18 +0200

    first commit v6

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,14 +1,16 @@
 "use client";
-
+// use client
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router"; // Correction du chemin d'importation pour useRouter
+import { useSWRConfig } from 'swr'; // Importation pour la revalidation des données
 
 import Form from "../../components/Form";
 
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
+  const { mutate } = useSWRConfig();  // Utilisation de mutate pour la revalidation
 
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
@@ -20,6 +22,7 @@ const CreatePrompt = () => {
     try {
       const response = await fetch("/api/prompt/new", {
         method: "POST",
+        headers: { 'Content-Type': 'application/json' }, // Assurez-vous d'inclure les headers
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
@@ -28,6 +31,7 @@ const CreatePrompt = () => {
       });
 
       if (response.ok) {
+        await mutate('/api/prompt'); // Revalidez les données du feed après la création réussie
         router.push("/");
       }
     } catch (error) {
